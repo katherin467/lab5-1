9f0a170b2a96870e154b996e28a1ba908d856545
Katherine Lim <k4lim@ucsd.edu>
2021-02-04 03:50:21 -0800

    lab 5 done

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.name h3").click(nameClick);
+}
+
+function nameClick(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,13 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	var name = request.query.name;
+	var description = request.query.description;
+	var friend = {
+					"name": name,
+					"description": description,
+					"imageURL": "http://lorempixel.com/400/400/people",		
+				 };
+	data.friends.push(friend);
+	response.render('index', data);
  }
